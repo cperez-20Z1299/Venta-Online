2c4e192bf5a8c4ca20c438dac686918541d93a66
Carlos Perez <cperez-2021299@kinal.edu.gt>
2023-02-08 15:42:38 -0600

    Subiendo archivos al repositorio

---
+++ README.md
@@ -0,0 +1,3 @@
+Este es un proyecto de una venta online 
+
+Instalar nodemon y agregar la carpeta .env para que el proyecto pueda ejecutarse
---
+++ models/categoria.js
@@ -0,0 +1,22 @@
+const{ Schema, model } = require('mongoose');
+
+const CategoriaSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    proveedor: {
+        type: String,
+        required: true
+    },
+    tipo: {
+        type: String,
+        required: true
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }
+});
+
+module.exports = model('Categoria',CategoriaSchema)
